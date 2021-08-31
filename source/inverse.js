8bf2575f31e783333a689b633819aa30b1ef0157
Lucifer <alex-orl1@mail.ru>
2021-09-01 02:07:49 +0300

    some fixes after review first iteration

--- source/inverse.js
+++ source/inverse.js
@@ -4,24 +4,26 @@
  * Reverse array
  * @param {Array} array that will be reversed
  * @param {number} ignore this number of elements; + from start and - from end
- * @returns {Array|null}
+ * @returns {Array|null} if args are correct, returns array with reversed part, else null
  */
 
 const inverse = (arr, num = 0) => {
-    if (!(Array.isArray(arr))) { // return undefined if array is not array
+    if (!Array.isArray(arr) || typeof num !== 'number') {
         return null;
     }
-    let arr_cp = arr.slice(); // make copy of array not to change anything in source
+    const arr_cp = arr.slice();
 
-    // arr_reversed will get part of array to revese, array will be without that part, then concat
+    if (num === 0) {
+        return arr_cp.reverse();
+    }
+
     if (num > 0) {
-        let arr_reversed = arr_cp.splice(num, arr_cp.length);
+        const arr_reversed = arr_cp.splice(num, arr_cp.length);
         arr_reversed.reverse();
         return arr_cp.concat(arr_reversed);
-    }
-    else {
-        let arr_reversed = arr_cp.splice(0, arr_cp.length + num);
+    } else {
+        const arr_reversed = arr_cp.splice(0, arr_cp.length + num);
         arr_reversed.reverse();
         return arr_reversed.concat(arr_cp);
     }
-}
+};
--- tests/inverse.js
+++ tests/inverse.js
@@ -46,4 +46,14 @@ QUnit.module('Тестируем функцию inverse', function () {
 		assert.deepEqual(inverse([ 1, 2, 3, 4, 5 ], 100500), [ 1, 2, 3, 4, 5 ]);
 		assert.deepEqual(inverse([ 1, 2, 3, 4, 5 ], -100500), [ 1, 2, 3, 4, 5 ]);
     });
+
+    QUnit.test('Функция возвращает null при некорректных аргументах', function (assert) {
+		assert.deepEqual(inverse([ 1, 2, 3, 4, 5 ], 'abc'), null);
+		assert.deepEqual(inverse(228, 1337), null);
+    });
+
+    QUnit.test('Функция корректно работает без второго аргумента и со вторым аргументом равным 0', function (assert) {
+        assert.deepEqual(inverse([ 1, 2, 3, 4, 5 ]), [ 5, 4, 3, 2, 1 ]);
+        assert.deepEqual(inverse([ 1, 2, 3, 4, 5 ], 0), [ 5, 4, 3, 2, 1 ]);
+    });
 });
